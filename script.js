f09127c803ceec2834ec8316c29ce97c23c30ac4
usoylu2 <45470527+usoylu2@users.noreply.github.com>
2023-03-19 23:05:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const socket = io();
 
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
-const messageContainer = document.getElementById('message-container');
+const messagesContainer = document.getElementById('messages');
 
+// Add event listener for form submit
 messageForm.addEventListener('submit', e => {
-	e.preventDefault();
-	const message = messageInput.value;
-	socket.emit('message', message);
-	messageInput.value = '';
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('newMessage', message);
+    messageInput.value = '';
 });
 
-socket.on('response', message => {
-	const messageElement = document.createElement('p');
-	messageElement.innerText = message;
-	messageContainer.append(messageElement);
-	messageContainer.scrollTop = messageContainer.scrollHeight;
+// Add event listener for new message
+socket.on('newMessage', message => {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.textContent = message;
+    messagesContainer.appendChild(messageElement);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
 });
+
